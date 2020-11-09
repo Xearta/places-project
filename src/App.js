3c7ad6f0c26cd63e9f2619a98f809160f991877c
Xearta <rsmcwow@gmail.com>
2020-11-09 10:21:02 -0500

    Added react-router-dom with some basic routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+import NewPlace from './places/pages/NewPlace';
 
-function App() {
-  return <h1>Let's get started!</h1>;
-}
+import Users from './user/pages/Users';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <Users />
+        </Route>
+        <Route path='/places/new' exact>
+          <NewPlace />
+        </Route>
+        <Redirect to='/' />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
