0315d973945d1d11171ef5ae5a558fd726c9b505
Xearta <rsmcwow@gmail.com>
2020-11-09 10:37:23 -0500

    Added usersList, userItem, and updated Users components

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Tanner Townsend',
+      image:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.proheadshots.ca%2Fwp-content%2Fuploads%2F2016%2F02%2Fdoctors-headshot.jpg&f=1&nofb=1',
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
